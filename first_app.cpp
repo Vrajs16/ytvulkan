6f3837c52ee9b0a08ea6eb3b698bcd2aa1ee0bd6
Vraj Shah <vrajs16108@gmail.com>
2022-08-18 21:27:20 -0400

    Ep 7: Fragment Interpolation

--- first_app.cpp
+++ first_app.cpp
@@ -46,15 +46,14 @@ namespace lve {
     }
 
     void FirstApp::loadModels() {
+        //std::vector<LveModel::Vertex> vertices{};
+        //sierpinski(vertices, 10, {0.0f, -0.5f}, {0.5f, 0.5f}, {-0.5f, 0.5f});
+        std::vector<LveModel::Vertex> vertices{
+                {{0.0f,  -0.5f}, {1.0f, 0.0f, 0.0f}},
+                {{0.5f,  0.5f},  {0.0f, 1.0f, 0.0f}},
+                {{-0.5f, 0.5f},  {0.0f, 0.0f, 1.0f}}
+        };
 
-        //        std::vector<LveModel::Vertex> vertices{
-        //                {{0.0f,  -0.5f}},
-        //                {{0.5f,  0.5f}},
-        //                {{-0.5f, 0.5f}}
-        //        };
-
-        std::vector<LveModel::Vertex> vertices{};
-        sierpinski(vertices, 10, {0.0f, -0.5f}, {0.5f, 0.5f}, {-0.5f, 0.5f});
         lveModel = std::make_unique<LveModel>(lveDevice, vertices);
     }
 
