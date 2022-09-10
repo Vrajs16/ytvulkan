d286447fb2d47c3bd4f38bf506c3ba80d2ce6107
Vraj Shah <vrajs16108@gmail.com>
2022-09-09 22:43:16 -0400

    Ep 14: Camera (View) Transform

--- simple_render_system.cpp
+++ simple_render_system.cpp
@@ -70,7 +70,7 @@ namespace lve
     void SimpleRenderSystem::renderGameObjects(VkCommandBuffer commandBuffer, std::vector<LveGameObject> &gameObjects, const LveCamera &camera)
     {
         lvePipeline->bind(commandBuffer);
-
+        auto projectionView = camera.getProjection() * camera.getView();
         for (auto &obj : gameObjects)
         {
             obj.transform.rotation.y = glm::mod(obj.transform.rotation.y + 0.01f, glm::two_pi<float>());
@@ -78,7 +78,7 @@ namespace lve
 
             SimplePushConstantData push{};
             push.color = obj.color;
-            push.transform = camera.getProjection() * obj.transform.mat4();
+            push.transform = projectionView * obj.transform.mat4();
 
             vkCmdPushConstants(
                 commandBuffer,
